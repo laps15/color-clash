a1828af4303f8beb28ef2a3ee7d87d93088f662d
Leonardo Strozzi <leo.strozzi98@gmail.com>
2025-03-21 00:05:39 -0300

    Increase splatter size

--- Player/player.gd
+++ Player/player.gd
@@ -65,6 +65,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
