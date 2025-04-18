6a8c41801665b66a91217efc89a3187f26609e9e
Leonardo Strozzi <leonardo@strozzi.dev>
2025-04-18 20:15:09 -0300

    Bugfix: Fix start game signal emitted before connected on game modes

--- Maps/level_map.gd
+++ Maps/level_map.gd
@@ -2,9 +2,18 @@ extends Node3D
 
 @export var draw_viewport: PackedScene = preload("res://Draw/draw_viewport.tscn")
 @export var custom_shader = preload("res://map_visual_shader.tres")
+@export var calculate_color_score: bool = false
 
 var mesh_viewport_map = {}
 var child_meshes = {}
+
+var color_score_thread: Thread
+var color_score_semaphore: Semaphore
+var hit_queue = []
+var viewport_mutex = {}
+var color_score = {}
+var color_score_by_mesh = {}
+var exit_thread = true
 
 var mesh_to_texture_map = {
 	"floor": "res://Maps/Resources/floor_texture.png",
@@ -12,12 +21,36 @@ var mesh_to_texture_map = {
 	"crate": "res://Maps/Resources/Wooden Crate_Crate_BaseColor.png",
 }
 
+func _exit_tree() -> void:
+	if not multiplayer.is_server() or not self.calculate_color_score:
+		return
+
+	self.viewport_mutex["STOP_THREAD"].lock()
+	self.exit_thread = true
+	self.viewport_mutex["STOP_THREAD"].unlock()
+	self.color_score_semaphore.post()
+
+	self.color_score_thread.wait_to_finish()
+
 func _ready():
 	for child in self.find_children("*", "MeshInstance3D", true):
 		self.init_mesh_properties(child as MeshInstance3D)
 
 	if self.is_multiplayer_authority():
 		ProcessProjectileCollisions.connect("map_hit", self.paint.rpc)
+
+	if multiplayer.is_server() and self.calculate_color_score:
+		self.color_score_thread = Thread.new()
+		self.color_score_semaphore = Semaphore.new()
+		self.viewport_mutex["STOP_THREAD"] = Mutex.new()
+		self.viewport_mutex["SCORE_UPDATE"] = Mutex.new()
+		self.exit_thread = false
+
+		for mesh_unique_id in self.mesh_viewport_map:
+			self.viewport_mutex[mesh_unique_id] = Mutex.new()
+			self.color_score_by_mesh[mesh_unique_id] = {}
+
+		self.color_score_thread.start(self._calculate_color_score)
 
 func init_mesh_properties(mesh_instance: MeshInstance3D):
 	var static_body = mesh_instance.get_node("StaticBody3D")
@@ -51,7 +84,15 @@ func paint(mesh_path: NodePath, pos: Vector2, color: Color = Color.RED):
 	var viewport_size = mesh_viewport_map[mesh_unique_id].size
 	var converted_pos = Vector2(pos[0] * viewport_size[0], pos[1] * viewport_size[1])
 	mesh_viewport_map[mesh_unique_id].paint(converted_pos, color)
-
+	
+	if multiplayer.is_server() and self.calculate_color_score:
+		if not color in self.color_score[mesh_unique_id]:
+			self.viewport_mutex[mesh_unique_id].lock()
+			self.color_score_by_mesh[mesh_unique_id][color] = 0
+			self.viewport_mutex[mesh_unique_id].unlock()
+		
+	#self.hit_queue.push_back(mesh_unique_id)
+	#self.color_score_semaphore.post()
 
 func get_color_at_pos(mesh_unique_id: int, pos: Vector2) -> Color:
 	var viewPort = mesh_viewport_map[mesh_unique_id] as SubViewport
@@ -60,3 +101,37 @@ func get_color_at_pos(mesh_unique_id: int, pos: Vector2) -> Color:
 		return Color.TRANSPARENT
 	
 	return color
+
+func _calculate_color_score() -> void:
+	while true:
+		self.color_score_semaphore.wait()
+
+		print("Semaphore post called")
+		self.viewport_mutex["STOP_THREAD"].lock()
+		if self.exit_thread:
+			self.viewport_mutex["STOP_THREAD"].unlock()
+			return
+		self.viewport_mutex["STOP_THREAD"].unlock()
+		
+		var hit_to_process = hit_queue.pop_front()
+		self.viewport_mutex[hit_to_process].lock()
+		var color_score_before = self.color_score_by_mesh[hit_to_process].duplicate()
+		var color_score_after = {}
+		for color in color_score_before:
+			color_score_after[color] = 0
+
+		var viewport = self.mesh_viewport_map[hit_queue] as SubViewport
+		var viewport_img = viewport.get_texture().get_image()
+
+		for x in range(viewport.size[0]):
+			for y in range(viewport.size[1]):
+				var color = viewport_img.get_pixel(x,y)
+				color_score_after[color] += 1
+		self.viewport_mutex[hit_to_process].unlock()
+		
+		self.viewport_mutex["UPDATE_SCORE"].lock()
+		for color in color_score_after:
+			if not color in self.color_score:
+				self.color_score[color] = 0
+			self.color_score[color] += color_score_after[color] - color_score_before[color]
+		self.viewport_mutex["UPDATE_SCORE"].unlock()
--- main.gd
+++ main.gd
@@ -1,18 +1,43 @@
 extends Node
 
-@export_file("*.tscn","*.scn") var game_scene_path: String
+@export_file("*.tscn","*.scn") var ffa_game_scene_path: String
+@export_file("*.tscn","*.scn") var teams_game_scene_path: String
 @export var landing_page: LandingPage
-@export var lobby_page: CanvasLayer
+@export var ffa_lobby_page: CanvasLayer
+@export var teams_lobby_page: CanvasLayer
+
+var game_scene_path: String
 
 func _ready() -> void:
-	self.lobby_page.hide()
+	self.landing_page.show()
+	self.ffa_lobby_page.hide()
+	self.teams_lobby_page.hide()
+
 	self.landing_page.done.connect(self._switch_to_lobby)
-	self.lobby_page.start_game.connect(self._start_game)
+	self.ffa_lobby_page.start_game.connect(self._start_game)
+	self.teams_lobby_page.start_game.connect(self._start_game)
+	self.game_scene_path = self.ffa_game_scene_path
+	
+	self.ffa_lobby_page.switch_game_mode_button.pressed.connect(self._on_switch_to_teams_pressed.rpc)
+	self.teams_lobby_page.switch_game_mode_button.pressed.connect(self._on_switch_to_ffa_pressed.rpc)
 
 func _switch_to_lobby() -> void:
 	self.landing_page.hide()
-	self.lobby_page.show()
+	self.ffa_lobby_page.show()
 
 func _start_game() -> void:
+	print("Calling start game: ", self.game_scene_path)
 	if self.game_scene_path:
 		LobbyService.load_game.rpc(self.game_scene_path)
+
+@rpc("call_local")
+func _on_switch_to_teams_pressed() -> void:
+	self.ffa_lobby_page.hide()
+	self.teams_lobby_page.show()
+	self.game_scene_path = self.teams_game_scene_path
+
+@rpc("call_local")
+func _on_switch_to_ffa_pressed() -> void:
+	self.teams_lobby_page.hide()
+	self.ffa_lobby_page.show()
+	self.game_scene_path = self.ffa_game_scene_path
